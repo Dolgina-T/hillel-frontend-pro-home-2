cd97beb68c2c731fb18a1b4a52792e637db988f9
Dolgina-T <101900680+Dolgina-T@users.noreply.github.com>
2022-03-22 15:46:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var name = prompt('Как тебя зовут?', 'Имя');
-var years = prompt('Сколько тебе лет?', '100');
-alert("Здравствуй, " + name + ', тебе ' + years + ' лет!');
+let name = prompt('Как тебя зовут?', 'Имя');
+let years = prompt('Сколько тебе лет?', '100');
+alert(`Здравствуй, ${name}, тебе ${years} лет!`);
 
